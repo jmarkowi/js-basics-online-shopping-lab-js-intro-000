5f9dbb6b86c576ed14daa3c897d83157daab5b82
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-13 03:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    var sum  = total()
+    return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
   }
 }
