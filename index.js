c78e09a937e96bc30a996d381fc2b0d816022058
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-13 03:26:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,11 +56,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  var total = total()
-  if(cardNumber === undefined) {
+  if(cardNumber === "") {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
