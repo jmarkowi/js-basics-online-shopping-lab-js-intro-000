c1d6e74f634ad1d2829b90b2872a994e31546f8f
Jesse Markowitz <jess.markowitz@gmail.com>
2020-04-13 02:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
     return "Your shopping cart is empty."
   }
   else if(numberItems === 1) {
-    return `In your cart you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }
 }
 
